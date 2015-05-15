4ff23f52d7e8ec30e058d5ad5cc42254572ec2cf
Guillaume Dott <guillaume+github@dott.fr>
2015-05-15 10:41:20 +0200

    Add thumbnail into article content

--- init.php
+++ init.php
@@ -1,12 +1,22 @@
 <?php
 class Af_Youtube_Thumbnail extends Plugin {
   function about() {
+    return array(0.1,
+      "Add the thumbnail for Youtube videos",
+      "gdott9");
   }
 
   function init($host) {
+    $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
   }
 
   function hook_article_filter($article) {
+    if(strpos($article["link"], "youtube.com") !== FALSE) {
+      $video_id = str_replace('http://www.youtube.com/watch?v=', '', $article["link"]);
+      $article["content"] = '<img src="http://img.youtube.com/vi/'.$video_id.'/hqdefault.jpg" alt="'.$article["title"].'" />';
+    }
+
+    return $article;
   }
 
   function api_version() { return 2; }
