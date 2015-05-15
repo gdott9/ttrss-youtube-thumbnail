21a1f54e60b676794ff8e4c11455f69d97074b3a
Guillaume Dott <guillaume+github@dott.fr>
2015-05-15 10:07:58 +0200

    First commit

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+class Af_Youtube_Thumbnail extends Plugin {
+  function about() {
+  }
+
+  function init($host) {
+  }
+
+  function hook_article_filter($article) {
+  }
+
+  function api_version() { return 2; }
+}
